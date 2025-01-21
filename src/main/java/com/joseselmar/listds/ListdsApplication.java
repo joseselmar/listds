c23702561787e8273bd17b593baea066eb9b1ef8
JoseSelmar <selmar.unimedbv@gmail.com>
2025-01-21 13:06:29 -0400

    criacao do projeto

---
+++ src/main/java/com/joseselmar/listds/ListdsApplication.java
@@ -0,0 +1,13 @@
+package com.joseselmar.listds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListdsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joseselmar/listds/ListdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseselmar.listds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
